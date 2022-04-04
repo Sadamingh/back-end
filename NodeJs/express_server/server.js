3801181168ad65c039b6f6e4dba51cfce3bb86cf
yxing11 <yxing11@usfca.edu>
2022-04-03 22:04:57 -0700

    update

--- NodeJs/express_server/server.js
+++ NodeJs/express_server/server.js
@@ -9,6 +9,10 @@ app.get("/contact", function(req, res) {
   res.send("Tel: XXXXXXXXXX")
 })
 
+app.get("/about", function(req, res) {
+  res.send("<h1>About me</h1>")
+})
+
 app.listen(3000, function() {
   console.log("Server started on port 3000.")
 });
