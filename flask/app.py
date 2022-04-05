d6ce74effefc1ab8c1370af09ddb80fbd5b2c430
yxing11 <yxing11@usfca.edu>
2022-04-05 13:43:06 -0700

    update

--- flask/app.py
+++ flask/app.py
@@ -14,5 +14,9 @@ def user(name):
 def admin():
     return redirect(url_for("home"))
 
+@app.route("/owner")
+def owner():
+    return redirect(url_for("user", name="owner!"))
+
 if __name__ == "__main__":
     app.run()
