9ff3310a94452cb9d6898dfdcfdd6e4915409e6a
yxing11 <yxing11@usfca.edu>
2022-04-05 13:45:28 -0700

    update

--- flask/app.py
+++ flask/app.py
@@ -1,10 +1,10 @@
-from flask import Flask, redirect, url_for
+from flask import Flask, redirect, url_for, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello"
+    return render_template("index.html", something="Hello world!")
 
 @app.route("/<name>")
 def user(name):
