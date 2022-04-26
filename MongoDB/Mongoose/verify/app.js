a3ea4ff5f4bb892ea6e9583d687b5c81c64ffaae
yxing11 <yxing11@usfca.edu>
2022-04-26 13:13:36 -0700

    update

---
+++ MongoDB/Mongoose/verify/app.js
@@ -0,0 +1,26 @@
+// jshint esversion:6
+
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost:27017/fruits');
+
+const fruitSchema = new mongoose.Schema ({
+  name: String,
+  score: {
+    type: Number,
+    min: 0,
+    max: 10
+  }
+});
+
+const Fruit = mongoose.model("Fruit", fruitSchema)
+
+Fruit.find(function(err, fruits){
+  if (err) {
+    console.log(err);
+  } else {
+    fruits.forEach(function(fruit) {
+      console.log(fruit.name);
+    })
+  }
+});
