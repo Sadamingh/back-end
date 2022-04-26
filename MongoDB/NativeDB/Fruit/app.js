f1b542066dbc9838a650cdd6a31e5e582ded01ec
yxing11 <yxing11@usfca.edu>
2022-04-26 10:16:58 -0700

    update

---
+++ MongoDB/NativeDB/Fruit/app.js
@@ -0,0 +1,66 @@
+const MongoClient = require('mongodb').MongoClient;
+const assert = require('assert');
+
+// Connection URL
+const url = 'mongodb://localhost:27017';
+
+// Database Name
+const dbName = 'fruits';
+
+// Create a new MongoClient
+const client = new MongoClient(url, { useNewUrlParser: true });
+
+// Use connect method to connect to the Server
+client.connect(function(err) {
+  assert.equal(null, err);
+  console.log("Connected successfully to server");
+
+  const db = client.db(dbName);
+
+  // insertDocuments(db, function() {
+  //   client.close();
+  // });
+
+  findDocuments(db, function() {
+    client.close();
+  });
+});
+
+// Create new data for the database
+const insertDocuments = function(db, callback) {
+  // Get the documents collection
+  const collection = db.collection('documents');
+  // Insert some documents
+  collection.insertMany([
+  {
+    name: "apple",
+    score: 5
+  },
+  {
+    name: "banana",
+    score: 8
+  },
+  {
+    name: "grape",
+    score: 2
+  }], function(err, result) {
+    assert.equal(err, null);
+    assert.equal(3, result.result.n);
+    assert.equal(3, result.ops.length);
+    console.log("Inserted 3 documents into the collection");
+    callback(result);
+  });
+}
+
+// find all the Documents
+const findDocuments = function(db, callback) {
+  // Get the documents collection
+  const collection = db.collection('documents');
+  // Find some documents
+  collection.find({}).toArray(function(err, fruits) {
+    assert.equal(err, null);
+    console.log("Found the following records");
+    console.log(fruits)
+    callback(fruits);
+  });
+}
